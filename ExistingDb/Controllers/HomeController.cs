aa98e0d8a647ee7b0b283a6b3d9061c9534f67bb
robertogamez <roberto.mb.gamez@hotmail.com>
2019-02-24 13:27:24 -0600

    Trabajando con scaffolding: Actualizar modelo por cambios en la bd

--- ExistingDb/Controllers/HomeController.cs
+++ ExistingDb/Controllers/HomeController.cs
@@ -2,15 +2,25 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ExistingDb.Models.Scaffold;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace ExistingDb.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly ZoomShoesDbContext context;
+
+        public HomeController(ZoomShoesDbContext ctx) => context = ctx;
+
         public IActionResult Index()
         {
-            return View();
+            return View(context.Shoes.Include(s => s.Color)
+                .Include(s => s.SalesCampaigns)
+                .Include(s => s.ShoeCategoryJunction)
+                .ThenInclude(junct => junct.Category)
+                .Include(s => s.Fitting));
         }
     }
 }
