67d1ac1aee8f37452b8fc318fa72bbc45b24b366
robertogamez <roberto.mb.gamez@hotmail.com>
2019-03-16 23:03:00 -0600

    Conceptos avanzados: Working with Keys

---
+++ AdvancedApp/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AdvancedApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AdvancedApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly AdvancedContext context;
+
+        public HomeController(AdvancedContext ctx)
+        {
+            this.context = ctx;
+        }
+
+        public IActionResult Index()
+        {
+            return View(context.Employees);
+        }
+
+        public IActionResult Edit(long id)
+        {
+            return View(id == default(long)
+            ? new Employee() : context.Employees.Find(id));
+        }
+
+        [HttpPost]
+        public IActionResult Update(Employee employee)
+        {
+            if (employee.Id == default(long))
+            {
+                context.Add(employee);
+            }
+            else
+            {
+                context.Update(employee);
+            }
+
+            context.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
---
+++ AdvancedApp/Migrations/20190317045716_Initial.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AdvancedApp.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Employees",
+                columns: table => new
+                {
+                    Id = table.Column<long>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    SSN = table.Column<string>(nullable: true),
+                    FirstName = table.Column<string>(nullable: true),
+                    FamilyName = table.Column<string>(nullable: true),
+                    Salary = table.Column<decimal>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employees", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Employees");
+        }
+    }
+}
